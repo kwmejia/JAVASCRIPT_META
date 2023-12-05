52cf5b2b8f8a201091328bb7b94de74edba00340
Kevin Mejia <kwmejia9@gmail.com>
2023-12-05 10:34:57 -0500

    Animaciones texto

--- 10 - Animaciones texto/js/app.js
+++ 10 - Animaciones texto/js/app.js
@@ -8,14 +8,48 @@ function maquinaEscribir() {
     textoMaquina.textContent = "";
     let textArr = palabras[currentIndex];
     textArr = textArr.split("");
-    console.log(textArr)
 
-    let i;
+    let i = 0;
 
     const pintarString = setInterval(() => {
-
-    }, 200);
+        textoMaquina.textContent += textArr[i];
+        i++;
+        if (i == textArr.length) {
+            clearInterval(pintarString);
+            setTimeout(() => {
+                borrarPalabra()
+            }, 1000); //Tiempo antes de borrar la palabra
+        }
+    }, 200); //Tiempo para escribir cada una de las letras
 }
 
+function borrarPalabra() {
+    let texto = textoMaquina.textContent;
+
+    const borrarString = setInterval(() => {
+        texto = texto.slice(0, -1);
+        textoMaquina.textContent = texto;
+        if (texto == "") {
+            clearInterval(borrarString);
+            currentIndex = (currentIndex + 1) % palabras.length; //Cambiar a la siguiente palabra
+            setTimeout(() => {
+                maquinaEscribir()
+            }, 500)
+        }
+    }, 100) //Tiempo en borrar cada una de las letras
+
+}
 
 maquinaEscribir()
+
+let textoAnimado = document.querySelector(".text");
+
+textoAnimado.innerHTML = textoAnimado.innerHTML
+    .split("")
+    .map((letra, index) =>
+        `<span 
+        style="transition-delay: ${index * 40}ms; filter: hue-rotate(${index * 30}deg);">
+          ${letra}
+        </span>`)
+    .join("")
+
