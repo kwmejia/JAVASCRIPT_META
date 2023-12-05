e75422a93fae309aa34d93859283d8c16f236572
Kevin Mejia <kwmejia9@gmail.com>
2023-12-05 06:52:48 -0500

    Animaciones con textos

--- 10 - Animaciones texto/js/app.js
+++ 10 - Animaciones texto/js/app.js
@@ -1,1 +1,21 @@
-console.log("Conectadooo")
+const textoMaquina = document.querySelector("#maquina_escribir_text");
+
+const palabras = ["Frontend", "Backend", "Ux/Iu", "Qa", "DevOps"];
+
+let currentIndex = 0;
+
+function maquinaEscribir() {
+    textoMaquina.textContent = "";
+    let textArr = palabras[currentIndex];
+    textArr = textArr.split("");
+    console.log(textArr)
+
+    let i;
+
+    const pintarString = setInterval(() => {
+
+    }, 200);
+}
+
+
+maquinaEscribir()
