634c18ccc0e57a8b3758f5abd38025bf72674286
Kevin Mejia <kwmejia9@gmail.com>
2023-12-06 07:39:21 -0500

    navidad

---
+++ 11 - Navidad/js/app.js
@@ -0,0 +1,12 @@
+const snowContainer = document.querySelector(".snow");
+
+snowContainer.innerHTML = "";
+
+function generarNieve() {
+    Array.from({ length: 51 }).forEach((_, i) => {
+
+    })
+
+}
+
+generarNieve();
