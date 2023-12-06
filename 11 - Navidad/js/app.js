4a010e5afc81fba48da93e4b35900234c8fcfb17
Kevin Mejia <kwmejia9@gmail.com>
2023-12-06 07:58:43 -0500

    navidad

--- 11 - Navidad/js/app.js
+++ 11 - Navidad/js/app.js
@@ -4,9 +4,14 @@ snowContainer.innerHTML = "";
 
 function generarNieve() {
     Array.from({ length: 51 }).forEach((_, i) => {
-
+        const num = generarNumero(11, 26)
+        snowContainer.innerHTML += `<span style="--i:${num}"></span>`
     })
 
+}
+
+function generarNumero(inicio, final) {
+    return Math.floor(Math.random() * (final - inicio + 1) + inicio)
 }
 
 generarNieve();
