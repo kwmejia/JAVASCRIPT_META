ef787a4ab68d926435b6ac32e6f86645bb8f8335
Kevin Mejia <kwmejia9@gmail.com>
2023-12-11 07:21:49 -0500

    Navidad terminado

--- 11 - Navidad/js/app.js
+++ 11 - Navidad/js/app.js
@@ -14,4 +14,53 @@ function generarNumero(inicio, final) {
     return Math.floor(Math.random() * (final - inicio + 1) + inicio)
 }
 
+function timerNavidad() {
+    //1. Obtener la fecha actual
+    const fechaActual = new Date();
+
+    //2. Definir el objetivo (25 de diciembre del mismo año)
+    const fechaObjetivo = new Date(fechaActual.getFullYear(), 11, 25) // El mes 11 representa  diciembre (0 - indice)
+
+    //3.Verificar si la fecha objetivo ya pasó
+    if (fechaActual.getMonth() === 11 && fechaActual.getDate() > 25) {
+        fechaObjetivo.setFullYear(fechaObjetivo.getFullYear() + 1);
+    }
+
+    //4.Calcular la diferencia entre fechas (en milisegundos)
+    let diferenciaEnMS = fechaObjetivo - fechaActual;
+
+
+    let diasRestantes = Math.floor(diferenciaEnMS / (1000 * 60 * 60 * 24));
+
+    let horasRestantes = Math.floor((diferenciaEnMS % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)))
+
+    let minutosRestantes = Math.floor(
+        (diferenciaEnMS % (1000 * 60 * 60) / (1000 * 60))
+    );
+
+    let segundosRestantes = Math.floor(
+        (diferenciaEnMS % (1000 * 60)) / 1000
+    );
+
+
+    console.log(diasRestantes, horasRestantes, minutosRestantes, segundosRestantes);
+
+    const diasHTML = document.getElementById("dias-restantes");
+    const horasHTML = document.getElementById("horas-restantes");
+    const minutosHTML = document.getElementById("minutos-restantes");
+    const segundosHTML = document.getElementById("segundos-restantes");
+
+
+    diasHTML.innerHTML = diasRestantes.toString().padStart(2, "0");
+
+    horasHTML.innerHTML = horasRestantes.toString().padStart(2, "0");
+
+    minutosHTML.innerHTML = minutosRestantes.toString().padStart(2, "0");
+
+    segundosHTML.innerHTML = segundosRestantes.toString().padStart(2, "0");
+
+}
+
+setInterval(timerNavidad, 1000);
+
 generarNieve();
