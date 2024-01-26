e68c6ee6aa1bb2edd55658fe1396ebbcfe0acb88
Kevin Mejia <kwmejia9@gmail.com>
2024-01-26 08:55:46 -0500

    login

--- 24 - Login/js/login/register.js
+++ 24 - Login/js/login/register.js
@@ -4,6 +4,8 @@ const userEmail = document.getElementById("userEmail");
 const userPassword = document.getElementById("userPassword");
 const passwordConfirmation = document.getElementById("passwordConfirmation");
 
+const URL = "http://localhost:3000/users";
+
 //Eventos
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -11,8 +13,46 @@ form.addEventListener("submit", (event) => {
     createUser()
 })
 
-function createUser() {
+async function createUser() {
     //Validar la información
+
+    if (!validatePassword()) {
+        showAlert("Contraseña no válida")
+        return
+    }
+    //Validar que el email no esté registrado
+    if (await validateEmail()) {
+        showAlert("El email ya se encuentra registrado.")
+        return
+    }
+
+    console.log("PASASTE LAS VALIDACIONES")
+
+    try {
+        //Crear al usuario
+        await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ email: userEmail.value, password: userPassword.value })
+        })
+    } catch (error) {
+        showAlert("Ocurrió un error al crear el usuario.")
+    }
+
+}
+
+function showAlert(msg) {
+    Swal.fire({
+        title: 'Error!',
+        text: msg,
+        icon: 'error',
+        showConfirmButton: false,
+        timer: 4000,
+        toast: "true",
+        position: "bottom-right"
+    })
 }
 
 
@@ -25,3 +65,19 @@ function validatePassword() {
     return userPassword.value === passwordConfirmation.value && regex.test(userPassword.value)
 }
 
+
+async function validateEmail() {
+
+    try {
+        //
+        const response = await fetch(`${URL}?email=${userEmail.value}`)
+
+        const data = await response.json()
+        //Si data tiene elementos quiere decir que el email ya está registrado
+        console.log(data)
+        return data.length
+    } catch (error) {
+        return false
+    }
+
+}
