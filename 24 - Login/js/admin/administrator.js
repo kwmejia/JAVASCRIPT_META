7b51ef142e3c0ca244dd8285c811e6fdb797166b
Kevin Mejia <kwmejia9@gmail.com>
2024-01-31 08:30:48 -0500

    crud

--- 24 - Login/js/admin/administrator.js
+++ 24 - Login/js/admin/administrator.js
@@ -7,10 +7,12 @@ const nameBook = document.getElementById("nameBook")
 const dateBook = document.getElementById("dateBook")
 const authorBook = document.getElementById("authorBook")
 const URLAuthors = "http://localhost:3000/authors"
+const URLBooks = "http://localhost:3000/books"
 //Eventos
 
 document.addEventListener("DOMContentLoaded", () => {
     loadSelectAuthor()
+    getBooks()
 })
 
 formAuthor.addEventListener("submit", (event) => {
@@ -19,9 +21,47 @@ formAuthor.addEventListener("submit", (event) => {
     createAuthor()
 })
 
-function loadSelectAuthor() {
+formBooks.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    createBook()
+})
+
+async function getBooks() {
+    const response = await fetch(`${URLBooks}?_embed=author`)
+    const data = await response.json()
+    console.log(data)
+}
+
+function createBook() {
+    const newBook = {
+        name: nameBook.value,
+        date: dateBook.value,
+        authorId: authorBook.value
+    }
+
+    fetch(URLBooks, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(newBook)
+    })
+}
+
+async function loadSelectAuthor() {
     //1.Obtener todos los autores -> GET
+    const response = await fetch(URLAuthors)
+    const data = await response.json()
     //2.Recorrer la lista de autores y por cada autor crear una etiqueta option, llenar el value y el titulo y agregarlo dentro del select
+
+    data.forEach(autor => {
+
+        const option = document.createElement("option")
+        option.value = autor.id
+        option.textContent = autor.name
+        authorBook.appendChild(option)
+    });
 }
 
 function createAuthor() {
