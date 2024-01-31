5be4d01e3885840658e569d3d35706c424f7cbfc
Kevin Mejia <kwmejia9@gmail.com>
2024-01-31 06:57:32 -0500

    login

--- 24 - Login/js/admin/administrator.js
+++ 24 - Login/js/admin/administrator.js
@@ -2,8 +2,16 @@
 const formAuthor = document.getElementById("formAuthor")
 const nameAuthor = document.getElementById("name")
 const ageAuthor = document.getElementById("age")
+const formBooks = document.getElementById("formBooks")
+const nameBook = document.getElementById("nameBook")
+const dateBook = document.getElementById("dateBook")
+const authorBook = document.getElementById("authorBook")
 const URLAuthors = "http://localhost:3000/authors"
 //Eventos
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadSelectAuthor()
+})
 
 formAuthor.addEventListener("submit", (event) => {
     event.preventDefault()
@@ -11,12 +19,17 @@ formAuthor.addEventListener("submit", (event) => {
     createAuthor()
 })
 
+function loadSelectAuthor() {
+    //1.Obtener todos los autores -> GET
+    //2.Recorrer la lista de autores y por cada autor crear una etiqueta option, llenar el value y el titulo y agregarlo dentro del select
+}
+
 function createAuthor() {
     fetch(URLAuthors, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify({ name: nameAuthor.value, age: ageAuthor.value })
+        body: JSON.stringify({ name: nameAuthor.value, age: formBooks.value })
     })
 }
